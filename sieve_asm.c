45272ea24b64d6f9f7aef494d53071c3e036b948
Maximilian CG <mcgoeritz@gmail.com>
2020-10-01 15:16:51 -0300

    KK parte 2

--- sieve_asm.c
+++ sieve_asm.c
@@ -3,20 +3,17 @@
 int is_prime(int n);
 
 int main() {
-	int prime = 1;
 	int num;
 
 	printf("Enter a cool number: ");
 	scanf("%d", &num);
-	printf("\n");
 
-	prime = is_prime(num);
-
-	if (prime == 0) {
-		printf("Your cool number is not prime!\n");
-	}
-	else {
+	if (is_prime(num)) {
 		printf("Your cool number is prime!\n");
 	}
+	else {
+		printf("Your cool number is not prime!\n");
+	}
+
 	return 0;
 }
