04f517d1bae2f55b1756402bb7a703b0d2d7da19
Maximilian CG <mcgoeritz@gmail.com>
2020-10-01 14:45:59 -0300

    Update repo

--- Makefile
+++ Makefile
@@ -2,11 +2,8 @@ sieve.o:
 	gcc -Wall -ansi -pedantic -o sieve sieve.c
 
 sieve_asm.o:
-	gcc -g -m32 -S sieve_asm.c -o sieve_asm.o
-	nasm is_prime.s -o is_prime.o
-	gcc sieve_asm.o -o sieve_asm
+	nasm -f elf32 is_prime.s -o is_prime.o
+	gcc -Wall -ansi -pedantic -m32 is_prime.o sieve_asm.c -o sieve_asm
+	rm is_prime.o
 
-teste:
-	nasm -f elf32 file.s -o file.out
-	ld -m elf_i386 file.out --entry=main -o file
 
--- is_prime.s
+++ is_prime.s
@@ -1,6 +1,66 @@
+global is_prime
+
+%use MASM
+
+section .text
+
 is_prime:
-        push    ebp
-        mov     ebp, esp
-        mov     eax, 0
-        pop     ebp
-        ret
+
+	; Chama da funcao
+	push ebp
+	mov ebp, esp
+
+	; Guardando os regs
+	push ebx
+	push edx
+
+
+	; i = 2
+	mov eax, 2
+
+FOR:
+	; i*i <= n
+	push eax
+	; imul -> eax = eax * argumento -> i*i
+	imul eax
+	mov ebx, DWORD PTR[ebp + 8]
+	cmp eax, ebx
+	ja PRIMO
+
+	; colocar n em eax
+	mov eax, ebx
+	cdq
+
+	; recuperar i para ebx
+	pop ebx
+
+	div ebx
+
+	; comaprar resto com 0
+	cmp edx, 0
+	je NPRIMO
+
+	inc ebx
+	mov eax, ebx
+	jmp FOR
+
+
+
+
+NPRIMO:
+	mov eax, 0
+	jmp FIM
+PRIMO:
+	mov eax, 1
+	jmp FIM
+FIM:
+	; Restaurar os regs
+	pop edx
+	pop ebx
+
+	; Saida da funcao
+	pop ebp
+	ret
+
+section .data
+
