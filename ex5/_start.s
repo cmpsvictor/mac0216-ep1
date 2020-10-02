4fe381fe86a6d008a789972b676869a5ceb5bd77
Maximilian CG <mcgoeritz@gmail.com>
2020-10-02 00:39:40 -0300

    UHUUUU START DEU CERTO!!!

--- ex5/_start.s
+++ ex5/_start.s
@@ -6,36 +6,16 @@ extern main
 
 section .text
 start:
-	push DWORD PTR[esp + 4]
-	push DWORD[esp + 4] ; colocar argc no stack
-	call main
-	jmp exit
-
-print:
 	push ebp
 	mov ebp, esp
-
-	push ecx
-	push edx
-
-	mov ecx, eax
-	mov edx, ebx
-	mov eax, 0x4
-	mov ebx, 0x1
-
-	int 0x80
-
-	pop edx
-	pop ecx
-
-	pop ebp
-	ret
+	mov eax , ebp
+	add eax, 8
+	push eax
+	push DWORD[ebp + 4] ; colocar argc no stack
+	call main
+	jmp exit
 
 exit:
 	mov ebx, eax
 	mov eax, 0x1
 	int 0x80
-
-section .data
-	mess db "oi", 0xA
-	messLen equ $-mess
